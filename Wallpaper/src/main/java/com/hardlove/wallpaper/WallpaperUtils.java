e5733a7b015bfba1b067be4c7b1a86d59eeadc89
chenlu <chenlu@126.com>
2022-08-30 18:12:59 +0800

    修复部分设备设置壁纸失败问题

--- Wallpaper/src/main/java/com/hardlove/wallpaper/WallpaperUtils.java
+++ Wallpaper/src/main/java/com/hardlove/wallpaper/WallpaperUtils.java
@@ -6,19 +6,18 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 
 import androidx.annotation.NonNull;
 
-import com.blankj.utilcode.util.ImageUtils;
 import com.blankj.utilcode.util.RomUtils;
-import com.blankj.utilcode.util.UriUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.hardlove.wallpaper.service.VideoWallpaperService;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 壁纸工具类
@@ -58,167 +57,188 @@ public class WallpaperUtils {
     }
 
     //=============================================================================================
-
-    /**
-     * 设置为桌面静态壁纸
-     *
-     * @param context
-     * @param bitmap
-     */
-    public static void setWallpaper(Context context, Bitmap bitmap) {
-        try {
-            WallpaperManager.getInstance(context).setBitmap(bitmap);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void setWallpaper(Context context, String path) {
+        setWallpaper(context, getImageContentUri(context, new File(path)));
     }
 
-    /**
-     * 调用系统图库，设置指定的图片为壁纸
-     *可选择设置桌面壁纸和锁屏壁纸
-     * @param context
-     * @param uriPath
-     */
-    public static void setWallpaper(Context context, Uri uriPath) {
-        if (RomUtils.isHuawei()) {
-            setHuawei(context, UriUtils.uri2File(uriPath).getAbsolutePath(), uriPath);
-        } else if (RomUtils.isXiaomi()) {
-            setXiaomi(context, UriUtils.uri2File(uriPath).getAbsolutePath(), uriPath);
-        } else if (RomUtils.isVivo()) {
-            setVivo(context, UriUtils.uri2File(uriPath).getAbsolutePath(), uriPath);
-        } else if (RomUtils.isOppo()) {
-            setOppo(context, UriUtils.uri2File(uriPath).getAbsolutePath(), uriPath);
-        } else {
-            setOthers(context, UriUtils.uri2File(uriPath).getAbsolutePath(), UriUtils.uri2File(uriPath));
+    public static void setWallpaper(Context context, Uri uri) {
+
+        try {
+            if (RomUtils.isHuawei()) {
+                setHuawei(context, uri);
+            } else if (RomUtils.isXiaomi()) {
+                setXiaomi(context, uri);
+            } else if (RomUtils.isVivo()) {
+                setVivo(context, uri);
+            } else if (RomUtils.isOppo()) {
+                setOppo(context, uri);
+            } else {
+                setOther(context, uri);
+            }
+        } catch (Exception e) {
+            try {
+                e.printStackTrace();
+                setWallpaperDefault2(context, uri);
+            } catch (Exception ex) {
+                try {
+                    ex.printStackTrace();
+                    setWallpaperDefault3(context, uri);
+                } catch (IOException exc) {
+                    exc.printStackTrace();
+                    ToastUtils.showShort("你是暂不支持");
+                }
+            }
         }
     }
 
     /**
      * 华为
      */
-    private static void setHuawei(Context context, String path, Uri uriPath) {
-        Intent intent;
+    private static void setHuawei(Context context, Uri uri) {
         try {
-            ComponentName componentName =
-                    new ComponentName("com.android.gallery3d", "com.android.gallery3d.app.Wallpaper");
-            intent = new Intent(Intent.ACTION_VIEW);
+            ComponentName componentName = new ComponentName("com.android.gallery3d", "com.android.gallery3d.app.Wallpaper");
+            Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setDataAndType(uriPath, "image/*");
+            intent.setDataAndType(uri, "image/*");
             intent.putExtra("mimeType", "image/*");
             intent.setComponent(componentName);
             context.startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
-            setWallpaperDefault(context, path);
+            setWallpaperDefault(context, uri);
         }
+
     }
 
     /**
      * 小米
      */
-    private static void setXiaomi(Context context, String path, Uri uriPath) {
-        Intent intent;
+    private static void setXiaomi(Context context, Uri uri) {
         try {
             ComponentName componentName = new ComponentName("com.android.thememanager",
                     "com.android.thememanager.activity.WallpaperDetailActivity");
-            intent = new Intent("miui.intent.action.START_WALLPAPER_DETAIL");
+            Intent intent = new Intent("miui.intent.action.START_WALLPAPER_DETAIL");
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setDataAndType(uriPath, "image/*");
+            intent.setDataAndType(uri, "image/*");
             intent.putExtra("mimeType", "image/*");
             intent.setComponent(componentName);
             context.startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
-            setWallpaperDefault(context, path);
+            setWallpaperDefault(context, uri);
         }
+
     }
 
     /**
      * Vivo
      */
-    private static void setVivo(Context context, String path, Uri uriPath) {
-        Intent intent;
+    private static void setVivo(Context context, Uri uri) {
         try {
-            ComponentName componentName =
-                    new ComponentName("com.vivo.gallery", "com.android.gallery3d.app.Wallpaper");
-            intent = new Intent(Intent.ACTION_VIEW);
+            ComponentName componentName = new ComponentName("com.vivo.gallery", "com.android.gallery3d.app.Wallpaper");
+            Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setDataAndType(uriPath, "image/*");
+            intent.setDataAndType(uri, "image/*");
             intent.putExtra("mimeType", "image/*");
             intent.setComponent(componentName);
             context.startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
-            setWallpaperDefault(context, path);
+            setWallpaperDefault(context, uri);
         }
+
     }
 
     /**
      * OPPO
      */
-    private static void setOppo(Context context, String path, Uri uriPath) {
-        Intent intent;
+    private static void setOppo(Context context, Uri uri) {
         try {
-            ComponentName componentName =
-                    new ComponentName("com.coloros.gallery3d", "com.oppo.gallery3d.app.Wallpaper");
-            intent = new Intent(Intent.ACTION_VIEW);
+            ComponentName componentName = new ComponentName("com.coloros.gallery3d", "com.oppo.gallery3d.app.Wallpaper");
+            Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setDataAndType(uriPath, "image/*");
+            intent.setDataAndType(uri, "image/*");
             intent.putExtra("mimeType", "image/*");
             intent.setComponent(componentName);
             context.startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
-            setWallpaperDefault(context, path);
+            setWallpaperDefault(context, uri);
         }
+
     }
+
+
+    public static void setOther(Context context, Uri uri) {
+        setWallpaperDefault(context, uri);
+    }
+
+    public static void setWallpaperDefault(Context context, Uri uri) {
+        Intent intent = new Intent(Intent.ACTION_ATTACH_DATA);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra("mimeType", "image/*");
+        intent.setDataAndType(uri, "image/*");
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            context.startActivity(intent);
+        } else {
+            throw new RuntimeException("setWallpaperDefault Intent.ACTION_ATTACH_DATA  设置壁纸失败...");
+        }
+
+    }
+
 
     /**
      * 其他
      */
-    private static void setOthers(Context context, String path, File file) {
-        try {
-            context.startActivity(WallpaperManager.getInstance(context.getApplicationContext())
-                    .getCropAndSetWallpaperIntent(getImageContentUri(context, file)));
-        } catch (Exception e) {
-            e.printStackTrace();
-            setWallpaperDefault(context, path);
+    private static void setWallpaperDefault2(Context context, Uri uri) {
+        Intent cropAndSetWallpaperIntent = WallpaperManager.getInstance(context.getApplicationContext()).getCropAndSetWallpaperIntent(uri);
+        if (cropAndSetWallpaperIntent.resolveActivity(context.getPackageManager()) != null) {
+            context.startActivity(cropAndSetWallpaperIntent);
+        } else {
+            throw new RuntimeException("setOthers 设置壁纸失败...");
         }
     }
 
     /**
      * 默认方法
      */
-    private static void setWallpaperDefault(Context context, String path) {
-        try {
-            WallpaperManager.getInstance(context.getApplicationContext()).setBitmap(ImageUtils.getBitmap(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static void setWallpaperDefault3(Context context, Uri uri) throws IOException {
+        InputStream inputStream = context.getContentResolver().openInputStream(uri);
+        WallpaperManager.getInstance(context.getApplicationContext()).setStream(inputStream);
+//            WallpaperManager.getInstance(context.getApplicationContext()).setBitmap(ImageUtils.getBitmap(UriUtils.uri2File(uri)));
     }
 
+
     /**
+     * 将图片插入到系统图库，修复部分设备无法加载图片的问题
+     *
      * @param context
      * @param imageFile
      * @return content Uri
      */
-    private static Uri getImageContentUri(Context context, File imageFile) {
+    public static Uri getImageContentUri(Context context, File imageFile) {
         String filePath = imageFile.getAbsolutePath();
         Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 new String[]{MediaStore.Images.Media._ID}, MediaStore.Images.Media.DATA + "=? ",
                 new String[]{filePath}, null);
+        Uri uri = null;
         if (cursor != null && cursor.moveToFirst()) {
             int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
-            Uri baseUri = Uri.parse("content://media/external/images/media");
-            return Uri.withAppendedPath(baseUri, "" + id);
+//            Uri baseUri = Uri.parse("content://media/external/images/media");
+            Uri baseUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            uri = Uri.withAppendedPath(baseUri, "" + id);
         } else {
             if (imageFile.exists()) {
                 ContentValues values = new ContentValues();
                 values.put(MediaStore.Images.Media.DATA, filePath);
-                return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-            } else {
-                return null;
+                uri = context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
             }
         }
+        if (cursor != null) {
+            cursor.close();
+        }
+
+        return uri;
     }
 }
