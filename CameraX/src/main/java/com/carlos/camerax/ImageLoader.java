edcbcd71afbd21d0dcfae541084333f53483a0b5
chenlu <chenlu@126.com>
2022-10-07 15:01:55 +0800

    。

--- CameraX/src/main/java/com/carlos/camerax/ImageLoader.java
+++ CameraX/src/main/java/com/carlos/camerax/ImageLoader.java
@@ -3,6 +3,8 @@ package com.carlos.camerax;
 import android.net.Uri;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+
 public interface ImageLoader {
-    void load(ImageView iv, Uri uri);
+    void load(@NonNull ImageView iv,@NonNull Uri uri);
 }
