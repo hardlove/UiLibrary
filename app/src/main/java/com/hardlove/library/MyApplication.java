d8413274f5b7d26bc9accb89c49533154355613a
chenlu <chenlu@126.com>
2022-10-07 14:34:07 +0800

    拍照demo

--- app/src/main/java/com/hardlove/library/MyApplication.java
+++ app/src/main/java/com/hardlove/library/MyApplication.java
@@ -3,13 +3,11 @@ package com.hardlove.library;
 import android.app.Application;
 import android.content.Context;
 
-import androidx.multidex.MultiDex;
 
 public class MyApplication  extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        MultiDex.install(base);
 
     }
 
