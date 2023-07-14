bcafdddfa8836ca569cdd4c352bdbb826a183454
chenlu <chenlu@126.com>
2023-07-14 10:56:23 +0800

    沉浸式状态栏的实现方式二

--- LocationPicker/src/main/java/com/hongwen/location/ui/LocationSelectActivity.kt
+++ LocationPicker/src/main/java/com/hongwen/location/ui/LocationSelectActivity.kt
@@ -2,11 +2,21 @@ package com.hongwen.location.ui
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Rect
 import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentActivity
 import com.hongwen.location.databinding.ActivityLocationSelectBinding
 
@@ -23,15 +33,54 @@ class LocationSelectActivity : FragmentActivity() {
 
         setContentView(bind.root)
 
-        // 设置状态栏透明
+        /**
+         * 沉浸式状态栏方法一：
+         * 1.添加
+         * <!--状态栏半透明,实现沉浸式状态栏-->
+         * <item name="android:windowTranslucentStatus">true</item>
+         * <!-- Status bar color.-->
+         * <item name="android:statusBarColor">@android:color/transparent</item>
+         * 2.onCreate 中添加
+         * if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+         * window?.apply {
+         * decorView.systemUiVisibility =
+         * View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+         * statusBarColor = Color.TRANSPARENT
+         * }
+         * }
+         */
+
+
+        /**
+         * 沉浸式状态栏方法二：
+         */
+        // 检查设备版本是否支持沉浸式状态栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window?.apply {
-                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                statusBarColor = Color.TRANSPARENT
-            }
+            setStatusBarColor()
+            setSystemUiVisibility()
         }
+
+
+
 
     }
 
+
+    // 设置状态栏颜色为透明
+    private fun setStatusBarColor() {
+        window?.statusBarColor = Color.TRANSPARENT
+    }
+
+    // 设置系统UI可见性以实现沉浸式状态栏效果
+    private fun setSystemUiVisibility() {
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            Log.d("Carlos","systemBarsInsets.bottom:"+systemBarsInsets.bottom+"  rect:"+systemBarsInsets)
+            WindowInsetsCompat.Builder(insets)
+                .setInsets(WindowInsetsCompat.Type.systemBars(),
+                    Insets.of(0, 0, 0, systemBarsInsets.bottom))
+                .build()
+        }
+    }
 
 }
