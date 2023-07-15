ae5ebf5213721753df6ba2a6a55781aa95dfee48
chenlu <chenlu@126.com>
2023-07-15 21:54:38 +0800

    .

--- LocationPicker/src/main/java/com/hongwen/location/adapter/LocationSelectAdapter.kt
+++ LocationPicker/src/main/java/com/hongwen/location/adapter/LocationSelectAdapter.kt
@@ -1,7 +1,11 @@
 package com.hongwen.location.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.hongwen.location.databinding.ItemSectionHotBinding
+import com.hongwen.location.databinding.ItemSectionLocationBinding
+import com.hongwen.location.databinding.ItemSectionNormalBinding
 import com.hongwen.location.holder.BindingViewHolder
 import com.hongwen.location.model.HotLocation
 import com.hongwen.location.model.LocatedLocation
@@ -30,13 +34,56 @@ class LocationSelectAdapter(private val allItems: List<Location>, val hotItems: 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
-        return BindingViewHolder()
+        when (viewType) {
+            VIEW_TYPE_LOCATION -> {
+                return BindingViewHolder(
+                    ItemSectionLocationBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+            VIEW_TYPE_HOT -> {
+                return BindingViewHolder(
+                    ItemSectionHotBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+            else -> {
+                return BindingViewHolder(
+                    ItemSectionNormalBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+
+
     }
 
-    override fun getItemCount() = 0
+    override fun getItemCount() = allItems.size
 
 
     override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
+        val location = allItems[holder.adapterPosition]
+        when (location) {
+            is LocatedLocation -> {
+
+            }
+            is HotLocation -> {
+
+            }
+            else -> {
+
+            }
+        }
+
     }
 
 }
