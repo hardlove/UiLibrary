2ad59c843e2c1651a4703552a37d618374bea2ea
chenlu <chenlu@126.com>
2023-10-28 20:34:49 +0800

    .

--- Network/src/main/java/com/hongwen/network/AdvApiManager.kt
+++ Network/src/main/java/com/hongwen/network/AdvApiManager.kt
@@ -1,50 +1,62 @@
 package com.hongwen.network
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 const val URL = "https://www.baidu.com"
 
 object AdvApiManager :
     ApiService by ServiceFactory.getService(url = URL, clazz = ApiService::class.java) {
 
-    suspend fun tet() {
-        this.getData("", 3, 1).transformResult(success = {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        runBlocking {
+            val apiService = ServiceFactory.getService(url = URL, clazz = ApiService::class.java)
+
+            // 1
+            apiService.getData("", 3, 1).transformResult(success = {
 
 
-        }, fail = {
-        })
+            }, fail = {
+
+            })
 
 
-        this.getData("", 1, 2).transform(success = {
+            // 2
+            apiService.getData("", 1, 2).transform(success = {
 
 
-        })
-        this.getData("", 1, 2)
-            .transformToResult()
-            .toThread(Dispatchers.Main)
-            .onError {
+            })
+
+
+            // 3
+            apiService.getData("", 1, 2)
+                .transformToResult()
+                .executeOnThread(Dispatchers.Main) {
+
+                }
+                .onError(Dispatchers.Main) {
+
+                }
+                .onSuccess(Dispatchers.Main) {
+
+                }
+            val result: Result<UserInfo> = apiService.getData("", 1, 2).transformToResult()
+            when (result) {
+                is Result.Success -> {
+
+                }
+
+                is Result.Error -> {}
 
             }
-            .onSuccess {
 
-            }
-        val result: Result<UserInfo> = this.getData("", 1, 2).transformToResult()
-        when (result) {
-            is Result.Success -> {
+            result.transform {
 
             }
 
-            is Result.Error -> {}
 
         }
-
-        result.transform {
-
-        }
-
-        this.getData("", 1, 3).transform(success = {
-
-        })
     }
 
 }
--- Network/src/main/java/com/hongwen/network/Result.kt
+++ Network/src/main/java/com/hongwen/network/Result.kt
@@ -1,6 +1,7 @@
 package com.hongwen.network
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 /*封装请求结果*/
@@ -12,30 +13,43 @@ sealed class Result<out T> {
 }
 
 /*定义扩展函数,用户处理成功情况*/
-inline fun <T> Result<T>.onSuccess(action: (T) -> Unit): Result<T> {
+suspend inline fun <T> Result<T>.onSuccess(
+    dispatcher: CoroutineDispatcher = Dispatchers.Main,
+    crossinline block: suspend (T) -> Unit,
+): Result<T> {
     if (this is Result.Success) {
-        action(data)
+        return withContext(dispatcher) {
+            block(data)
+            this@onSuccess
+        }
     }
     return this
 }
 
 /*定义扩展函数,用户处理失败情况*/
-inline fun  <T> Result<T>.onError(action: (Code) -> Unit): Result<T> {
+suspend inline fun <T> Result<T>.onError(
+    dispatcher: CoroutineDispatcher = Dispatchers.Main,
+    crossinline block: suspend (Code) -> Unit,
+): Result<T> {
     if (this is Result.Error) {
-        action(exception)
+        return withContext(dispatcher) {
+            block(exception)
+            this@onError
+        }
     }
     return this
 }
 
-suspend fun <T> T.toThread(dispatcher: CoroutineDispatcher): T {
-
-    return withContext(dispatcher){
-
-
-        this@toThread
+suspend inline fun <T> Result<T>.executeOnThread(
+    dispatcher: CoroutineDispatcher,
+    crossinline block: suspend () -> Unit,
+): Result<T> {
+    withContext(dispatcher) {
+        block()
     }
-
+    return this
 }
+
 fun <T> T.toIoThread(): T {
 
     return this
