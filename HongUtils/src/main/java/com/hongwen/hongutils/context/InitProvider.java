e754262ba0fd737f034c64210d83e0a1389e1649
chenlu <chenlu@126.com>
2023-11-29 18:38:24 +0800

    转场动画

--- HongUtils/src/main/java/com/hongwen/hongutils/context/InitProvider.java
+++ HongUtils/src/main/java/com/hongwen/hongutils/context/InitProvider.java
@@ -1,30 +1,74 @@
 package com.hongwen.hongutils.context;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Application;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.lang.ref.WeakReference;
 import java.util.Objects;
 
 public class InitProvider extends ContentProvider {
-    @SuppressLint("StaticFieldLeak")
-    private static Context context;
+    private static Application context;
+    private static WeakReference<Activity> currentActivity;
 
 
     public static Context getApplicationContext() {
         return context;
     }
 
+    public static Activity getCurrentActivity() {
+        return currentActivity.get();
+    }
+
 
     @Override
     public boolean onCreate() {
-        context = Objects.requireNonNull(getContext()).getApplicationContext();
+        context = (Application) Objects.requireNonNull(getContext()).getApplicationContext();
+
+        context.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                currentActivity = new WeakReference<>(activity);
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
         return true;
     }
 
